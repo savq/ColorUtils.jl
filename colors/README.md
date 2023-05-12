b27798d51dc062362b94bbdd5e99112763f68245
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2023-05-12 16:57:23 -0500

    Fix xterm color generation
    - Use views instead of manual indexing.
    - Fix one-off error in color cube.
    - Add function to save generated colors.
    - Use plain text instead of julia files for Vim's named colors and
      xterm's list of colors.

---
+++ colors/README.md
@@ -0,0 +1,3 @@
+- The Vim named colors are listed in [`neovim/src/nvim/highlight_group.c`](https://github.com/neovim/neovim/blob/master/src/nvim/highlight_group.c).
+- The xterm colors are generated programmatically in `src/color256.jl`.
+  The original colors can be found in [`xterm/256colres.h`](https://github.com/xterm-x11/xterm-snapshots/blob/master/256colres.h).
--- src/color8bit.jl
+++ src/color8bit.jl
@@ -1,37 +1,38 @@
 """
     generate_xterm_colors()::Vector{URgb}
 
-Compute the array of true color (URgb) equivalents for xterm's 256 color palette (Color256).
+Compute the vector of true color (RGB) equivalents for xterm's 256 color palette (Color256).
 """
 function generate_xterm_colors()::Vector{URgb}
     colors = Vector(undef, 256)
-    _fill_16_colors!(colors)
-    _fill_color_cube!(colors)
-    _fill_gray_ramp!(colors)
+    _fill_16_colors!(@view colors[1:16])
+    _fill_color_cube!(@view colors[17:232])
+    _fill_gray_ramp!(@view colors[233:256])
     return colors
 end
 
 function _fill_16_colors!(colors)
     i = 0
-    💡 = false:true
-    for factor in [1, 0]
+    💡 = [false, true]
+    for factor in [0x1, 0x0]
         for b in 💡, g in 💡, r in 💡
             colors[i+=1] = URgb(
-                r && 255 >>> factor + factor,
-                g && 255 >>> factor + factor,
-                b && 255 >>> factor + factor,
+                r && (255 >>> factor) + factor,
+                g && (255 >>> factor) + factor,
+                b && (255 >>> factor) + factor,
             )
         end
     end
 
+    # Dark white is lighter than light black
     colors[9] = colors[8]
-    colors[8] = URgb([colors[8]...] .+ 64 ...)
+    colors[8] = URgb([colors[8]...] .+ 0x40 ...)
     return
 end
 
 function _fill_color_cube!(colors)
-    i = 16
-    💡 = 1:5
+    i = 0
+    💡 = 0:5
     for r in 💡, g in 💡, b in 💡
         colors[i+=1] = URgb(
             r != 0 && r * 40 + 55,
@@ -42,12 +43,18 @@ function _fill_color_cube!(colors)
 end
 
 function _fill_gray_ramp!(colors)
-    colors[233:256] = map(0:23) do gray
+    for gray in 0:23
         val = gray * 10 + 8
-        URgb(val, val, val)
+        colors[gray + 1] = URgb(val, val, val)
     end
-    return
 end
 
 const XTERM_COLORS = generate_xterm_colors()
 
+function save_colors(path, colors)
+    open(path, "w+") do f
+        for c in colors
+            println(f, c)
+        end
+    end
+end
