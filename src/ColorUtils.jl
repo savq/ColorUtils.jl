572bf24bffe1a0fdc8c30a86d8137ffb035ef117
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2025-12-09 12:43:08 -0500

    Refactor quantization
    - Update `quantize` to return an index-value tuple
    - Move quantize functions to `distance.jl`

--- src/ColorUtils.jl
+++ src/ColorUtils.jl
@@ -47,10 +47,7 @@ include("./apca.jl")
 using .APCA: get_contrast
 
 include("./distance.jl")
-using .ColorDistance: get_distance
-
-include("./quantization.jl")
-using .Quantization: quantize, quantize_to_term_colors
+using .ColorDistance: get_distance, quantize, quantize_to_term_colors
 
 include("./io.jl")
 using .ColorIO: hex, @rgb_str
--- src/distance.jl
+++ src/distance.jl
@@ -2,16 +2,27 @@ module ColorDistance
 
 using ..ColorUtils: AbstractColor
 using ..OklabColors: Oklab
+using ..TermColors: XTERM_COLORS
 
 """
     get_distance(color1::Oklab, color2::Oklab)
 
 Calculate the color difference ΔEOK of two colors in Oklab color space.
 """
-function get_distance(c1::Oklab, c2::Oklab)
-    return hypot(c1.L - c2.L, c1.a - c2.a, c1.b - c2.b)
-end
+get_distance(c1::Oklab, c2::Oklab) = hypot(c1.L - c2.L, c1.a - c2.a, c1.b - c2.b)
 
 get_distance(c1::AbstractColor, c2::AbstractColor) = get_distance(Oklab(c1), Oklab(c2))
+
+
+"""
+    quantize(color, palette)
+
+Return the index and the value of the `palette` color that best approximates `color`.
+"""
+quantize(color, palette) = argmin(idx_color -> get_distance(color, idx_color[2]), enumerate(palette))
+
+quantize_to_term_colors(color) = quantize(color, XTERM_COLORS)
+
+# [RGB24(r + r << 4, g + g << 4, b + b << 4) for r in 0x0:0xF for g in 0x0:0xF for b in 0x0:0xF]
 
 end # module
